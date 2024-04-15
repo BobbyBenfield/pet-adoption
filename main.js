d6d3ee65a9cea862df4ad6508a72a3895706d18f
Bobby <robert.t.benfield@gmail.com>
2024-04-15 14:33:31 +0900

    pulledin real dynamic data

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ async function start() {
   const ourTemperature = weatherData.properties.periods[0].temperature
   document.querySelector("#temperature-output").textContent = ourTemperature
 }
+
 start()
 
 
 async function petsArea() {
-  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsPromise = await fetch("https://cute-biscuit-98cf6f.netlify.app/.netlify/functions/pets")
   const petsData = await petsPromise.json()
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
